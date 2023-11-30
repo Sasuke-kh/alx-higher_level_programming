a46c8fa145dc278cd67e8d211d02d7b2c69f8df4
Sasuke-kh <omerkhalid56682@gmail.com>
2023-11-30 16:01:53 +0200

    update

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    counter = len(argv)
+    count = 0
+    if counter == 1:
+        print("{} arguments.".format(counter - 1))
+    else:
+        print("{} arguments:".format(counter - 1))
+        for count in range(counter):
+            if count == 0:
+                continue
+            print("{} : {}".format(count, argv[count]))
